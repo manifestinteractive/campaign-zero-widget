d0a6980fafd4d493b92b7b826ed33c34301fb6d4
Peter Schmalfeldt <peter@manifestinteractive.com>
2016-04-23 10:38:34 -0400

    Backend connection to API working to get representative data for zip code. Based on this article https://sunlightfoundation.com/blog/2012/01/19/dont-use-zipcodes/ I opted to not use actual zipcodes, but rather a zipcode to latitude and longitude conversion.

---
+++ config.php
@@ -0,0 +1,4 @@
+<?php
+
+define('API_KEY', '9d5d2b3a12cb4dc28e4f174b805c647c');
+define('API_URL_LEGISLATORS', 'http://openstates.org/api/v1/legislators/geo/');
--- widget.js
+++ widget.js
@@ -6,6 +6,10 @@
   var jqueryVersion = '1.12.3';
   var scriptTag;
   var elementName = 'campaign-zero-widget';
+  var geoEnabled = null;
+  var geoLocation = {};
+  var geoError = null;
+  var timeout = null;
 
   /** Get reference to self (scriptTag) */
   var allScripts = document.getElementsByTagName('script');
@@ -58,6 +62,183 @@
     main();
   }
 
+  /** Get Geo Location */
+  function getLocation() {
+    if(geoLocation && geoLocation.latitude && geoLocation.longitude){
+      geoSuccess({
+        coords: geoLocation
+      });
+
+    } else if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(geoSuccess, geoError);
+      geoEnabled = true;
+    } else {
+      geoEnabled = false;
+    }
+  }
+
+  /** Geo Location Success */
+  function geoSuccess(position) {
+    geoLocation = position.coords;
+    getRepresentatives(geoLocation);
+  }
+
+  /** Geo Location Error */
+  function geoError(error) {
+    switch(error.code) {
+      case error.PERMISSION_DENIED:
+        geoError = 'Denied Request for Geolocation.';
+        break;
+      case error.POSITION_UNAVAILABLE:
+        geoError = 'Location Information Unavailable.';
+        break;
+      case error.TIMEOUT:
+        geoError = 'Location Request Timed Out.';
+        break;
+      case error.UNKNOWN_ERROR:
+        geoError = 'Unable to Determine Location.';
+        break;
+    }
+
+    showError(geoError);
+  }
+
+  function showError(error){
+    var elm = jQuery('#' + elementName);
+    jQuery('small.note', elm).html('<i class="fa fa-exclamation-triangle"></i>&nbsp; ' + error).addClass('error animated shake');
+    jQuery('button.submit', elm).removeAttr('disabled').html('Find your rep');
+    clearTimeout(timeout);
+    timeout = setTimeout(function(){
+      jQuery('small.note', elm).removeClass('error animated shake').html('leave empty to use your current location');
+    }, 5000);
+  }
+
+  /** Load Initial Widget Form */
+  function loadForm(){
+    var elm = jQuery('#' + elementName);
+    elm.html('');
+
+    var html = '<div class="wrapper animated fadeIn" style="display: none">'+
+      '<h2>End Police Violence</h2>' +
+      '<p class="intro">Where does your rep stand?</p>' +
+      '<form id="campaign-zero-form">' +
+      '<input autocomplete="off" type="text" name="location" id="zip-code" placeholder="Enter your Zip Code" minlength="5" maxlength="5" onkeyup="this.value=this.value.replace(/[^0-9]/g,\'\');">' +
+      '<small class="note">leave empty to use your current location</small>' +
+      '<button class="submit" type="submit">Find your rep</button>' +
+      '</form>' +
+      '<small class="powered-by"><span>Powered by </span><a href="http://www.joincampaignzero.org/" target="_blank">Campaign Zero</a></small>' +
+      '<a class="add-to-site" href="https://github.com/manifestinteractive/campaign-zero-widget" target="_blank">Add <span>this</span> to <span>your</span> site</a>' +
+      '</div>';
+
+    elm.append(html);
+
+    setTimeout(function(){
+      jQuery('.wrapper', elm).show();
+
+      jQuery('#campaign-zero-form').submit(function(event) {
+
+        jQuery('button.submit', elm).attr('disabled', 'disabled').html('<i class="fa fa-circle-o-notch fa-spin fa-fw"></i> Loading');
+
+        var zipcode = jQuery('#zip-code').val();
+        var pattern = /[0-9]{5}/g;
+
+        if(zipcode !== '' && pattern.test(zipcode)){
+          getRepresentatives(null, zipcode);
+        } else if(zipcode !== '' && !pattern.test(zipcode)) {
+          showError('Invalid Zip Code ( e.g. 90210 )');
+        } else if(zipcode === '') {
+          getLocation();
+        }
+
+        event.preventDefault();
+      });
+    }, 200);
+  }
+
+  /** Generate Results */
+  function generateResults(response){
+    var elm = jQuery('#' + elementName);
+
+    jQuery('.wrapper', elm).removeClass('animated fadeIn');
+    jQuery('.wrapper', elm).addClass('animated fadeOut');
+
+    setTimeout(function(){
+      elm.html('');
+
+      var backgroundImage = 'https://maps.googleapis.com/maps/api/staticmap?center='+ response.location.latitude +','+ response.location.longitude +'&zoom=10&maptype=roadmap&size=800x600&sensor=false&style=feature:administrative|visibility:off&style=feature:landscape.natural.terrain|visibility:off&style=feature:poi|visibility:off&style=element:labels|visibility:off&style=feature:road|element:labels|visibility:off&style=feature:transit|visibility:off&style=feature:road|element:geometry|visibility:simplified|color:0x999999&style=feature:water|element:geometry|color:0xcccccc&style=feature:landscape|element:geometry.fill|color:0xaaaaaa';
+      var html = '<div class="wrapper animated fadeIn" style="display: none; min-height: 298px; background: url('+ backgroundImage +') center center no-repeat; background-size: cover;">';
+
+      if(response && response.results.length === 1){
+
+      } else if (response && response.results.length >= 1){
+        html += '<h2 class="black">Pick a Representative</h2>';
+        html += '<ul>';
+
+        for (var key in response.results) {
+          if (response.results.hasOwnProperty(key)) {
+            var val = response.results[key];
+            html += '<li><a href="javascript:void(0)" class="representative-summary" data-id="'+ key +'">' +
+              '<div class="avatar ' + val.party.toLowerCase() + '" style="background-image: url(' + val.photo_url + ')"></div>' +
+              '<div class="summary">'+
+                '<div class="summary-name ' + val.party.toLowerCase() + '">' + val.full_name + '</div>' +
+                '<div class="summary-details">Party: ' + val.party + ' &nbsp;|&nbsp; District: ' + val.district + '</div>' +
+              '</div>' +
+              '</a></li>';
+          }
+        }
+
+        html += '</ul></div>';
+
+        html += '<small class="powered-by back"><a href="javascript:void(0);"><i class="fa fa-angle-left"></i>&nbsp; Back</a></small>';
+
+        elm.append(html);
+
+        setTimeout(function(){
+          jQuery('.wrapper', elm).show();
+          jQuery('small.back a', elm).click(function(){ loadForm(); });
+          jQuery('a.representative-summary', elm).click(function(){
+            var id = jQuery(this).data('id');
+            var representative = response.results[id];
+            console.log(representative);
+          });
+        }, 200);
+
+      } else if (response && response.results.length === 0){
+
+      }
+
+    }, 200);
+  }
+
+  /** Handle Form Submission */
+  function getRepresentatives(geoLocation, zipCode){
+
+    var jsonpUrl = 'index.php';
+    if(geoLocation){
+      jsonpUrl += '?latitude=' + geoLocation.latitude + '&longitude=' + geoLocation.longitude
+    } else if(zipCode){
+      jsonpUrl += '?zipcode=' + zipCode
+    } else {
+      return false;
+    }
+
+    jQuery.ajax({
+      url: jsonpUrl,
+      type: 'GET',
+      dataType: 'json',
+      success: function(response) {
+        if(response && response.error){
+          showError(response.error);
+        } else {
+          generateResults(response);
+        }
+      },
+      error: function(jqXHR, textStatus, errorThrown) {
+        showError('ERROR: ' + errorThrown);
+      }
+    });
+  }
+
   /** starting point for widget */
   function main() {
 
@@ -71,33 +252,8 @@
         jQuery('<div id="' + elementName + '"></div>').insertBefore(scriptTag);
       }
 
-      var elm = jQuery('#' + elementName);
-      var html = '<div class="wrapper" style="display: none">'+
-        '<h2>End Police Violence</h2>' +
-        '<p class="intro">Where does your rep stand?</p>' +
-        '<form id="campaign-zero-form">' +
-        '<input type="text" name="location" placeholder="Zip Code or Name">' +
-        '<button>Find your rep</button>' +
-        '</form>' +
-        '<small class="powered-by"><span>Powered by </span><a href="http://www.joincampaignzero.org/" target="_blank">Campaign Zero</a></small>' +
-        '<a class="add-to-site" href="https://github.com/manifestinteractive/campaign-zero-widget" target="_blank">Add <span>this</span> to <span>your</span> site</a>' +
-        '</div>';
-
-      elm.append(html);
-      jQuery('.wrapper', elm).fadeIn();
-
-      //or you could wait until the page is ready
-
-      //example jsonp call
-      //var jsonp_url = 'www.example.com/jsonpscript.js?callback=?';
-      //jQuery.getJSON(jsonp_url, function(result) {
-      //	alert('win');
-      //});
-
-      //example script load
-      //loadScript('http://example.com/anotherscript.js', function() { /* loaded */ });
+      loadForm();
     });
-
   }
 
 })();
